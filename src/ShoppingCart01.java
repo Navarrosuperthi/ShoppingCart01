fa59c34a21863d10565ed58f43115f275aabc4d0
FabioBritto <fabio.tritono@gmail.com>
2024-06-05 14:12:23 -0300

    Commit project ShoppingCart01

---
+++ src/ShoppingCart01.java
@@ -0,0 +1,14 @@
+public class ShoppingCart01 {
+    public static void main(String[] args) {
+
+        String itemDesc = "Camisa";
+        String custName = "Alex";
+        String message = custName + "Quer comprar uma camisa" + itemDesc;
+        
+        System.out.println(message);
+        
+        
+       
+        
+    }
+}
